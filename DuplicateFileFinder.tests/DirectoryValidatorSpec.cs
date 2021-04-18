33a2b9e90b0eb1ae651d308748d64851ed557da1
Matthew Adams <matt@matthewladams.com>
2021-04-18 12:33:15 +1000

    Add some basic test coverage

--- DuplicateFileFinder.tests/DirectoryValidatorSpec.cs
+++ DuplicateFileFinder.tests/DirectoryValidatorSpec.cs
@@ -11,5 +11,12 @@ namespace DuplicateFileFinder.tests
             var validatorResponse = DirectoryValidator.ValidatePath(AppDomain.CurrentDomain.BaseDirectory);
             Assert.False(validatorResponse.Error);
         }
+
+        [Fact]
+        public void InvalidDirectoryShouldError()
+        {
+            var validatorResponse = DirectoryValidator.ValidatePath("qwertyuiop");
+            Assert.True(validatorResponse.Error);
+        }
     }
 }
